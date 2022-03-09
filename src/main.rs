e61d10471a7b9903e035fb830a978bfd1dd1a4f3
xutianyi <xutianyi1999@live.com>
2022-03-09 12:39:57 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,28 @@ use std::net::SocketAddr;
 use std::str::FromStr;
 
 use log4rs::append::console::ConsoleAppender;
-use log4rs::Config;
 use log4rs::config::{Appender, Root};
+use log4rs::Config;
 use log4rs::encode::pattern::PatternEncoder;
 use log::LevelFilter;
 use tokio::net::{TcpListener, TcpStream};
 
 fn logger_init() {
     let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("[Console] {d(%Y-%m-%d %H:%M:%S)} - {l} - {m}{n}")))
+        .encoder(Box::new(PatternEncoder::new(
+            "[Console] {d(%Y-%m-%d %H:%M:%S)} - {l} - {m}{n}",
+        )))
         .build();
 
     let config = Config::builder()
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(Root::builder().appender("stdout").build(LevelFilter::Info)).unwrap();
+        .build(
+            Root::builder().appender("stdout").build(
+                LevelFilter::from_str(&std::env::var("RUST_LOG").unwrap_or(String::from("INFO")))
+                    .unwrap(),
+            ),
+        )
+        .unwrap();
 
     log4rs::init_config(config).unwrap();
 }
@@ -28,7 +36,7 @@ fn logger_init() {
 fn zip<T1, T2, E1, E2>(a: Result<T1, E1>, b: Result<T2, E2>) -> Option<(T1, T2)> {
     match (a, b) {
         (Ok(a), Ok(b)) => Some((a, b)),
-        _ => None
+        _ => None,
     }
 }
 
@@ -40,7 +48,9 @@ async fn main() {
 
     let args: Vec<String> = args.collect();
 
-    let map: HashMap<SocketAddr, SocketAddr> = args.iter().map(|str| str.split_once("->"))
+    let map: HashMap<SocketAddr, SocketAddr> = args
+        .iter()
+        .map(|str| str.split_once("->"))
         .filter(Option::is_some)
         .map(Option::unwrap)
         .map(|(a, b)| zip(SocketAddr::from_str(a), SocketAddr::from_str(b)))
@@ -60,16 +70,17 @@ async fn main() {
                     let (mut source_stream, peer_addr) = listener.accept().await?;
 
                     tokio::spawn(async move {
-                        info!("Peer addr {} forward to {}", peer_addr, dest);
+                        debug!("{} forward to {}", peer_addr, dest);
 
                         let res = async move {
                             let mut dest_stream = TcpStream::connect(dest).await?;
                             tokio::io::copy_bidirectional(&mut source_stream, &mut dest_stream).await?;
                             Result::<(), io::Error>::Ok(())
-                        }.await;
+                        }
+                            .await;
 
                         if let Err(e) = res {
-                            error!("Peer addr {} forward error: {:?}", peer_addr, e)
+                            error!("{} forward error: {:?}", peer_addr, e)
                         }
                     });
                 }
